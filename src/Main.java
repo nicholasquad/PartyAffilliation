0002341e7dbf4c772f5ef05ec03c45e051c09dc4
nicholasquad <nicholasmash2006@gmail.com>
2024-09-27 12:36:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        String partyAfil = "";
+
+        //Create a scanner and read in the PartyAfil
+
+        if(partyAfil.equalsIgnoreCase( "R"))
+        {
+            System.out.println("You get a Reblican Elephant");
+        }
+        else if(partyAfil.equalsIgnoreCase( "D")){
+        {
+            System.out.println("You get an Elephant");
+        }
+
+
+            }
+        {else(partyAfil.equalsIgnoreCase( "I")){
+                System.out.println("You get a Man");
+
+
+            }
+}
